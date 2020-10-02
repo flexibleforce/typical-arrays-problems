027eced7b58d7c9803bfd1f512524e783bd11d30
flexibleforce <69279947+flexibleforce@users.noreply.github.com>
2020-10-02 21:02:40 +0300

    feat: implement typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length < 1 ) return 0;
+    return (array.sort((a, b) => b - a))[array.length-1];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length < 1 ) return 0;
+    return (array.sort((a, b) => a - b))[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length < 1 ) return 0;
+    return avg = (array.reduce((a, b) => a + b, 0) / array.length);
 }
